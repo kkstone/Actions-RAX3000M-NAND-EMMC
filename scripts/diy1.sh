7ebea38f076551435c9c8bfde73929c7d3daca84
kkstone <10545609+kkstone@users.noreply.github.com>
2024-04-23 21:29:37 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -3,7 +3,8 @@
 # Add a feed source
 
 cd package
-git clone https://github.com/sbwml/openwrt_helloworld.git
+git clone https://github.com/kenzok8/small.git
+git clone https://github.com/kuoruan/openwrt-frp.git
 git clone https://github.com/sundaqiang/openwrt-packages.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
