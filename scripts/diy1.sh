033725d1d0cb90fbb28cb753dba63840089d2017
kkstone <10545609+kkstone@users.noreply.github.com>
2024-04-23 19:01:49 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -5,4 +5,5 @@
 cd package
 git clone https://github.com/kenzok8/small.git
 git clone https://github.com/sundaqiang/openwrt-packages.git
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
