6aa06de82e93ede0524eeb932b051b317a96f672
kkstone <10545609+kkstone@users.noreply.github.com>
2024-04-23 18:48:43 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -2,7 +2,7 @@
 
 # Add a feed source
 
-#cd package
-#git clone https://github.com/kenzok8/small.git
-#git clone https://github.com/sundaqiang/openwrt-packages.git
-#git clone https://github.com/jerrykuku/lua-maxminddb.git
+cd package
+git clone https://github.com/kenzok8/small.git
+git clone https://github.com/sundaqiang/openwrt-packages.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
