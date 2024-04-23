22f1ad4147319207c99d2140af9ff14ca70cf317
kkstone <10545609+kkstone@users.noreply.github.com>
2024-04-23 18:15:56 +0800

    Create diy1.sh

---
+++ scripts/diy1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Add a feed source
+
+#cd package
+#git clone https://github.com/kenzok8/small.git
+#git clone https://github.com/sundaqiang/openwrt-packages.git
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
