2f33d7c0fd039b24ca74e86aaee5251226281a86
kkstone <10545609+kkstone@users.noreply.github.com>
2024-04-26 14:36:14 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -5,10 +5,10 @@
 cd package
 git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kuoruan/openwrt-frp.git
-git clone https://github.com/sundaqiang/openwrt-packages.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 
 wget https://github.com/kkstone/office-work/raw/main/luci-app-frp.zip
 unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
-cp openwrt-packages/luci-app-wolplus/po/zh-cn openwrt-packages/luci-app-wolplus/po/zh_Hans
+wget https://github.com/kkstone/office-work/raw/main/luci-app-wolplus.zip
+unzip luci-app-wolplus.zip && rm -rf luci-app-wolplus.zip
