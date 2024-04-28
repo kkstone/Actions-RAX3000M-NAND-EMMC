e2fc38760201cc9b1083d796cfd286218b7cbf4b
kkstone <10545609+kkstone@users.noreply.github.com>
2024-04-28 13:11:41 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -8,7 +8,7 @@ git clone https://github.com/kuoruan/openwrt-frp.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 
-wget https://github.com/kkstone/office-work/raw/main/luci-app-frp.zip
+wget https://github.com/kkstone/Actions-RAX3000M-eMMC/raw/main/luci-packages/luci-app-frp.zip
 unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
-wget https://github.com/kkstone/office-work/raw/main/luci-app-wolplus.zip
+wget https://github.com/kkstone/Actions-RAX3000M-eMMC/raw/main/luci-packages/luci-app-wolplus.zip
 unzip luci-app-wolplus.zip && rm -rf luci-app-wolplus.zip
