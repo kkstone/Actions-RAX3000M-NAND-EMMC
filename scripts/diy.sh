fb49b30b98befd6b6bc71c5ad29dca7f1c7fddf6
kkstone <10545609+kkstone@users.noreply.github.com>
2024-07-18 10:34:22 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -5,3 +5,7 @@
 
 #更改默认源地址为上海交大源
 sed -i "s,mirrors.vsean.net/openwrt,mirrors.sjtug.sjtu.edu.cn/immortalwrt,g" package/emortal/default-settings/files/99-default-settings-chinese
+
+#为5.4内核添加iptables-mod-socket
+wget https://github.com/yuos-bit/AutoBuild-OpenWrt/raw/main/patchs/5.4/iptables-mod-socket.patch -O package/iptables-mod-socket.patch
+patch -p1 < package/iptables-mod-socket.patch
