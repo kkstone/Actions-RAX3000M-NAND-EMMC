d9d6eaf67378dffb8ec633068a3ad89060c55b7c
kkstone <10545609+kkstone@users.noreply.github.com>
2024-08-23 11:48:11 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -7,6 +7,7 @@ git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kuoruan/openwrt-frp.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/immortalwrt/homeproxy.git
 
 wget https://github.com/kkstone/Actions-RAX3000M-eMMC/raw/main/luci-packages/luci-app-frp.zip
 unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
