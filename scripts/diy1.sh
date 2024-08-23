9bc8b2b452508b2e13d871a13b657ef81fe6c4dd
kkstone <10545609+kkstone@users.noreply.github.com>
2024-08-23 12:03:48 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -9,7 +9,7 @@ git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/immortalwrt/homeproxy.git
 
-wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/main/luci-packages/luci-app-frp.zip
+wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-frp.zip
 unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
-wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/main/luci-packages/luci-app-wolplus.zip
+wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-wolplus.zip
 unzip luci-app-wolplus.zip && rm -rf luci-app-wolplus.zip
