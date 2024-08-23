614685fcd94e244a45ac37fcc2c8b48b8c9fee21
kkstone <10545609+kkstone@users.noreply.github.com>
2024-08-23 13:10:48 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -7,6 +7,9 @@ git clone https://github.com/kuoruan/openwrt-frp.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/immortalwrt/homeproxy.git
+git clone https://github.com/mwarning/zerotier-openwrt.git
+cp -rf zerotier-openwrt/zerotier zerotier/
+rm -rf zerotier-openwrt/
 
 wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-frp.zip
 unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
