a80204f8fbbea6ba007deaaaa8c1a07941788bf9
kkstone <10545609+kkstone@users.noreply.github.com>
2024-08-28 16:31:31 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -5,3 +5,9 @@
 
 #更改默认源地址为上海交大源
 sed -i "s,mirrors.vsean.net/openwrt,mirrors.sjtug.sjtu.edu.cn/immortalwrt,g" package/emortal/default-settings/files/99-default-settings-chinese
+
+#添加kmod-ipt-socket等Passwall必须模块
+cp -rf $GITHUB_WORKSPACE/patchs/iptables-makefile.patch $GITHUB_WORKSPACE/openwrt/p1.patch
+patch -p1 < $GITHUB_WORKSPACE/openwrt/p1.patch
+cp -rf $GITHUB_WORKSPACE/patchs/netfilter.patch $GITHUB_WORKSPACE/openwrt/p2.patch
+patch -p1 < $GITHUB_WORKSPACE/openwrt/p2.patch
