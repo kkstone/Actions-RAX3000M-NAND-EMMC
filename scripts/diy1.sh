76966327a28f5d9a3636b579a380c25bcf755367
kkstone <10545609+kkstone@users.noreply.github.com>
2024-08-28 14:19:30 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -7,7 +7,7 @@ git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kuoruan/openwrt-frp.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/muink/luci-app-natmapt.git
 
-wget https://github.com/vernesong/OpenClash/archive/master.zip && unzip master.zip && rm -rf master.zip && cp -rf OpenClash-master/luci-app-openclash luci-app-openclash/ && rm -rf OpenClash-master/
 wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-frp.zip && unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
 wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-wolplus.zip && unzip luci-app-wolplus.zip && rm -rf luci-app-wolplus.zip
